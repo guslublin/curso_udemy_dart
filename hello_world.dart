828f807fafe019fde2245f94d0b0da72ce7f8c8b
Gustavo Lublin <guslublin@gmail.com>
2023-12-22 12:46:49 -0300

    Hello world dart

---
+++ hello_world.dart
@@ -0,0 +1,10 @@
+main(){
+  var number = 1;
+  print('Hello world Dart');
+  printNumero(number);
+}
+
+printNumero(int num){
+  // print('Valor de número:' + numero.toString());
+  print('Valor de número:${num.toString()}');
+}
